04193abdf458f99ddc32fbf1848e814041fa3594
adamjsturge <adamjsturge@gmail.com>
2024-10-18 08:09:39 -0700

    Start catppuccin tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,21 @@
-<div class="container mx-auto p-4">
+<div class="ctp-container mx-auto p-4">
 	<h1 class="mb-4 text-2xl font-bold">Tools Menu</h1>
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-3">
 		<a
 			href="/base64/encode"
-			class="block rounded-lg bg-blue-500 p-6 text-center text-white shadow-md hover:bg-blue-700"
+			class="hover:bg-ctp-blue-dark block rounded-lg bg-ctp-blue p-6 text-center text-black shadow-md"
 		>
 			Base64 Encode
 		</a>
 		<a
 			href="/base64/decode"
-			class="block rounded-lg bg-green-500 p-6 text-center text-white shadow-md hover:bg-green-700"
+			class="hover:bg-ctp-green-dark block rounded-lg bg-ctp-green p-6 text-center text-black shadow-md"
 		>
 			Base64 Decode
 		</a>
 		<a
 			href="/qr-code"
-			class="block rounded-lg bg-red-500 p-6 text-center text-white shadow-md hover:bg-red-700"
+			class="hover:bg-ctp-red-dark block rounded-lg bg-ctp-red p-6 text-center text-black shadow-md"
 		>
 			QR Code
 		</a>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,41 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
-	theme: {
-		extend: {
-			colors: {
-			  mocha: {
-				base: '#1e1e2e',
-				mantle: '#181825',
-				crust: '#11111b',
-				text: '#cdd6f4',
-				subtext0: '#bac2de',
-				subtext1: '#a6adc8',
-				overlay0: '#6c7086',
-				overlay1: '#585b70',
-				overlay2: '#45475a',
-				surface0: '#313244',
-				surface1: '#292c3c',
-				surface2: '#24273a',
-				blue: '#89b4fa',
-				lavender: '#b4befe',
-				sapphire: '#74c7ec',
-				sky: '#89dceb',
-				teal: '#94e2d5',
-				green: '#a6e3a1',
-				yellow: '#f9e2af',
-				peach: '#fab387',
-				maroon: '#eba0ac',
-				red: '#f38ba8',
-				mauve: '#cba6f7',
-				pink: '#f5c2e7',
-				flamingo: '#f2cdcd',
-				rosewater: '#f5e0dc',
-			  },
-			},
-		  },
-	},
-
-	plugins: [require('@tailwindcss/typography')]
+	safelist: [
+		{
+			pattern: /bg-.+/
+		},
+		'mocha',
+		'macchiato',
+		'frappe',
+		'latte'
+	],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require("@catppuccin/tailwindcss")({
+			// prefix to use, e.g. `text-pink` becomes `text-ctp-pink`.
+			// default is `false`, which means no prefix
+			prefix: "ctp",
+			// which flavour of colours to use by default, in the `:root`
+			defaultFlavour: "frappe",
+		}),
+	]
 };
