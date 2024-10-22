459790e23a8da7220a20a802e51aa2ff6833f0c6
adamjsturge <adamjsturge@gmail.com>
2024-10-21 21:04:00 -0700

    Fix the menu colors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,31 +3,31 @@
 	<div class="grid grid-cols-1 gap-4 md:grid-cols-3">
 		<a
 			href="/base64/encode"
-			class="block rounded-lg bg-ctp-red p-6 text-center text-ctp-text shadow-md hover:bg-ctp-maroon font-bold"
+			class="block rounded-lg bg-ctp-surface0 p-6 text-center text-ctp-text shadow-md hover:bg-ctp-surface1 font-bold"
 		>
 			Base64 Encode
 		</a>
 		<a
 			href="/base64/decode"
-			class="block rounded-lg bg-ctp-blue p-6 text-center text-ctp-text shadow-md hover:bg-ctp-sapphire font-bold"
+			class="block rounded-lg bg-ctp-surface0 p-6 text-center text-ctp-text shadow-md hover:bg-ctp-surface1 font-bold"
 		>
 			Base64 Decode
 		</a>
 		<a
 			href="/qr-code"
-			class="block rounded-lg bg-ctp-mauve p-6 text-center text-ctp-text shadow-md hover:bg-ctp-pink font-bold"
+			class="block rounded-lg bg-ctp-surface0 p-6 text-center text-ctp-text shadow-md hover:bg-ctp-surface1 font-bold"
 		>
 			QR Code
 		</a>
 		<a
 			href="/url/encode"
-			class="block rounded-lg bg-ctp-green p-6 text-center text-ctp-text shadow-md hover:bg-ctp-olive font-bold"
+			class="block rounded-lg bg-ctp-surface0 p-6 text-center text-ctp-text shadow-md hover:bg-ctp-surface1 font-bold"
 			>
 			URL Encode
 		</a>
 		<a
 			href="/url/decode"
-			class="block rounded-lg bg-ctp-yellow p-6 text-center text-ctp-text shadow-md hover:bg-ctp-sunflower font-bold"
+			class="block rounded-lg bg-ctp-surface0 p-6 text-center text-ctp-text shadow-md hover:bg-ctp-surface1 font-bold"
 			>
 			URL Decode
 		</a>
